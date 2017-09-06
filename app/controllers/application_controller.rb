32e533c16594eab3d2bfd151047d67bb034a70a1
francomendoza <franco@lingolive.com>
2017-09-05 22:41:09 -0400

    add root path and response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def root
+    render json: {
+      status: "SUCCESS"
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'application#root'
   post '/missions/create', to: 'missions#create'
   get '/missions', to: 'missions#index'
   post '/missionstatus/update', to: 'missions#update_status'
